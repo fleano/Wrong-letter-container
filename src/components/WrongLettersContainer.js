1ed037e93095562d07a273efea6eb9f81c61281f
Justin Looi <justinlooijw@gmail.com>
2022-05-25 05:12:26 +0800

    Make wrong letters update with wrong letters in database

---
+++ src/components/WrongLettersContainer.js
@@ -0,0 +1,27 @@
+import "./styes.css";
+
+function WrongLettersContainer(props) {
+  const letterChosen = props.letterChosen;
+  const wrongLetters = props.wrongLetters;
+
+  function ListItem(props) {
+    //  There is no need to specify the key here:
+    return <li className="grave-letter">{props.value}</li>;
+  }
+
+  function WrongLetters(props) {
+    const listItems = props.wrongLetters.map((number, index) => (
+      // Key should be specified inside the array.
+      <ListItem key={index.toString()} value={number} />
+    ));
+    return <ul>{listItems}</ul>;
+  }
+
+  return (
+    <div id="letter-graveyard" className="col-sm-4">
+      <WrongLetters wrongLetters={wrongLetters} />
+    </div>
+  );
+}
+
+export default WrongLettersContainer;
